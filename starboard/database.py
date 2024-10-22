ece70265f05b8a147bd140629f03e5fca0cb00f3
ArcasCZ <VydraMan@seznam.cz>
2024-10-22 21:45:58 +0200

    [Starboard] Karma proxy

--- starboard/database.py
+++ starboard/database.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from sqlalchemy import BigInteger, Integer, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
-from pie.database import database
+from pie.database import database, session
 
 VERSION = 1
 
@@ -24,6 +26,32 @@ class StarboardMessage(database.base):
         starboard_message_id,
         name="source_message_id_starboard_message_id_unique",
     )
+
+    @staticmethod
+    def get(
+        guild_id: int,
+        author_id: int = None,
+        source_channel_id: int = None,
+        source_message_id: int = None,
+        starboard_channel_id: int = None,
+        starboard_message_id: int = None,
+    ) -> Optional[StarboardChannel]:
+
+        query = session.query(StarboardMessage).filter_by(guild_id=guild_id)
+
+        if author_id:
+            query = query.filter_by(author_id=author_id)
+        if source_channel_id:
+            query = query.filter_by(source_channel_id=source_channel_id)
+        if source_message_id:
+            query = query.filter_by(source_message_id=source_message_id)
+        if starboard_channel_id:
+            query = query.filter_by(starboard_channel_id=starboard_channel_id)
+        if starboard_message_id:
+            query = query.filter_by(starboard_message_id=starboard_message_id)
+
+        return query.one_or_none()
+
 
 class StarboardChannel(database.base):
     __tablename__ = "boards_starboard_channels"
--- starboard/module.py
+++ starboard/module.py
@@ -3,13 +3,75 @@ from discord.ext import commands
 
 from pie.bot import Strawberry
 
+from database import StarboardMessage, StarboardSource
+
 from ..karma.module import Karma
 
 
 class Starboard(commands.Cog):
 
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: Strawberry = bot
+        self.starboard_channels = []
+        self.source_channels = []
+
+    # Listeners
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, reaction: discord.RawReactionActionEvent):
+        """Handle added reactions."""
+        if reaction.channel_id in self.source_channels:
+            await self._process_reaction(reaction)
+        elif reaction.channel_id in self.starboard_channels:
+            await self._proxy_karma(reaction, added=True)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, reaction: discord.RawReactionActionEvent):
+        """Handle removed reactions."""
+        if reaction.channel_id in self.starboard_channels:
+            await self._proxy_karma(reaction, added=False)
+
+    # Commands
+
+    pass
+
+    # Helper functions
+
+    async def _proxy_karma(self, reaction: discord.RawReactionActionEvent, added: bool):
+        message: StarboardMessage = StarboardMessage.get(
+            guild_id=reaction.guild_id, starboard_message_id=reaction.message_id
+        )
+        if not message:
+            return
+
+        karma: Karma = self.bot.get_cog(Karma.qualified_name)
+        if not karma:
+            return
+
+        emoji_value = karma.get_emoji_value(
+            guild_id=reaction.guild_id, emoji=reaction.emoji
+        )
+
+        if emoji_value == 0:
+            return
+
+        if added:
+            karma.reaction_added(
+                guild_id=reaction.guild_id,
+                msg_author_id=message.author_id,
+                react_author_id=reaction.member.id,
+                emoji_value=emoji_value,
+            )
+        else:
+            karma.reaction_removed(
+                guild_id=reaction.guild_id,
+                msg_author_id=message.author_id,
+                react_author_id=reaction.member.id,
+                emoji_value=emoji_value,
+            )
+
+    async def _process_reaction(reaction: discord.RawReactionActionEvent):
+        pass
 
 
 async def setup(bot: Strawberry) -> None:
